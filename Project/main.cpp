d58f2d95d8c08678f41ec451fc3e28eacd87c5e0
CtrlShift-Esc <jehovah0427@gmail.com>
2022-09-26 13:01:38 +0900

    update

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,38 +1,17 @@
 #include <string>
 #include <vector>
-#include <sstream>
-//#include <algorithm>
 
 using namespace std;
 
-string solution(string s) {
-	string answer = "";
-	stringstream ss(s);
-	string out;
-	int min = 9999999, max = -9999999;
-	while (getline(ss, out, ' '))
-	{
-		int idx = 0;
-		if (out[0] == '-')
-			idx = 1;
-
-		int n = stoi(out.substr(idx, out.length()));
-		if (idx)
-			n *= -1;
-		if (min > n)
-			min = n;
-		if (max < n)
-			max = n;
-	}
-	answer += to_string(min);
-	answer += " ";
-	answer += to_string(max);
-
+int solution(vector<int> a, vector<int> b) {
+	int answer = 0;
+	for (int i = 0; i < a.size(); i++)
+		answer += a[i] * b[i];
 	return answer;
 }
 
 void main()
 {
 	//test
-	//auto ret = solution("-1 -2 -3 -4");
+	//auto ret = solution({-1,0,1}, {1,0,-1});
 }
